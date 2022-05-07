2223e451643c43b507fcdf4c13da8c0b0cff1ad6
Alejandro jose Ruzafa casas <aruzafa-@c2r9s6.42malaga.com>
2022-05-07 22:09:09 +0200

    Fix null bug at lstclear

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 13:31:41 by aruzafa-          #+#    #+#             */
-/*   Updated: 2022/04/25 14:34:27 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2022/05/07 22:07:45 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*next;
 
-	next = (*lst)->next;
 	while (*lst)
 	{
 		next = (*lst)->next;

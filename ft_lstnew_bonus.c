73d9bece758f9376daf50ce886048be0597f2c29
Alejandro jose Ruzafa casas <aruzafa-@c2r10s6.42malaga.com>
2022-05-08 14:12:06 +0200

    lstnew use calloc instead of malloc

--- ft_lstnew_bonus.c
+++ ft_lstnew_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/25 13:16:21 by aruzafa-          #+#    #+#             */
-/*   Updated: 2022/04/25 14:29:11 by aruzafa-         ###   ########.fr       */
+/*   Updated: 2022/05/08 14:11:40 by aruzafa-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,9 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*res;
 
-	res = (t_list *) malloc(sizeof(t_list));
+	res = (t_list *) ft_calloc(1, sizeof(t_list));
 	if (!res)
 		return (0);
 	res->content = content;
-	res->next = 0;
 	return (res);
 }

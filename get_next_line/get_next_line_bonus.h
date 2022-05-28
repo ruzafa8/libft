8b55ca1f1cfa55a7b7811cdd65f88457f8339e86
Alejandro jose Ruzafa casas <aruzafa-@c2r10s4.42malaga.com>
2022-05-28 18:54:37 +0200

    Added get next line and printf functions

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aruzafa- <aruzafa-@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/08 18:23:21 by aruzafa-          #+#    #+#             */
+/*   Updated: 2022/05/28 18:26:31 by aruzafa-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*ft_get_next_line(int fd);
+void	gnl_strjoin(char **s1, char *s2, size_t s2_len);
+
+#endif
